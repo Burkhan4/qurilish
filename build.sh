2cbed0a0c20dce2c584656a0e441d24c746cc1d7
Burkhan4 <alexandrey670@gmail.com>
2025-06-04 11:33:59 +0300

    version 1.5

--- build.sh
+++
@@ -1,6 +0,0 @@
-# Install dependencies with --no-cache-dir to reduce size
-pip install --no-cache-dir -r requirements.txt
-
-# Collect static files and apply migrations
-python manage.py collectstatic --noinput
-python manage.py migrate
